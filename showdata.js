02b4c055a0d554c60833121a8d57262a4e453e41
Sumit Dubey <149804568+Sumit0ubey@users.noreply.github.com>
2024-03-06 10:36:51 +0530

    Update showdata.js

--- showdata.js
+++ showdata.js
@@ -5,22 +5,19 @@ function fetchData() {
             const parser = new DOMParser();
             const htmlDoc = parser.parseFromString(this.responseText, 'text/html');
             
-            // Extract data from the fetched HTML content
             const firstName = htmlDoc.getElementById('fname').value || '-';
             const lastName = htmlDoc.getElementById('lname').value || '-';
             const phone = htmlDoc.getElementById('phone').value || '-';
 
-            // Display the extracted data in the specified IDs
             document.getElementById("fillfname").innerText = firstName;
             document.getElementById("filllname").innerText = lastName;
             document.getElementById("fillphone").innerText = phone;
         }
     };
-    xhttp.open("GET", "form.html", true); // Specify the HTML file to fetch data from
+    xhttp.open("GET", "form.html", true);
     xhttp.send();
 }
 
-// Call the fetchData function when the page loads
 window.onload = function() {
     fetchData();
 };
